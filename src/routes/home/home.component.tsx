d64e8f479b1b0b6c0d0d45cffd288bc5a31ec185
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-02-03 17:27:05 -0800

    created PageSection component

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,11 +1,16 @@
+// internal imports
 import HeroSlideshow from "../../components/hero-slideshow/hero-slideshow.component";
+import { PageSection } from "../../components/page-section/page-section.component";
 
 // images
 import { HeroImages } from "../../utils/images";
 
+// components
 const Home = () => {
   return (
-    <HeroSlideshow images={ HeroImages } timing={500} />
+    <PageSection>
+      <HeroSlideshow images={HeroImages} timing={500} />
+    </PageSection>
   );
 };
 
