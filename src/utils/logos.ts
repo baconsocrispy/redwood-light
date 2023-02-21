0577045d6128a7b2b464052db52906892dd5eacc
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-02-21 13:53:59 -0800

    removed various logos

--- src/utils/logos.ts
+++ src/utils/logos.ts
@@ -2,12 +2,9 @@
 import AIGLogo from '../assets/images/partner-logos/aig-logo.png';
 import AmazonLogo from '../assets/images/partner-logos/amazon-logo.png';
 import FoxLogo from '../assets/images/partner-logos/fox-logo.png';
-import GriffinMediaLogo from '../assets/images/partner-logos/griffin-media-logo.png';
 import LARamsLogo from '../assets/images/partner-logos/la-rams-logo.png';
 import LATimesLogo from '../assets/images/partner-logos/la-times-logo.png';
 import NBCUniversalLogo from '../assets/images/partner-logos/nbc-universal-logo.png';
-import PimcoLogo from '../assets/images/partner-logos/pimco-logo.png';
-import TegnaLogo from '../assets/images/partner-logos/tegna-logo.png';
 import TelemundoLogo from '../assets/images/partner-logos/telemundo-logo.png';
 import TitansLogo from '../assets/images/partner-logos/tennessee-titans-logo.png';
 import UFCLogo from '../assets/images/partner-logos/ufc-logo.png';
@@ -35,12 +32,6 @@ export const ClientLogos: Image[] = [
     alt: "Fox Logo",
   },
   {
-    id: 24,
-    name: "Griffin Media Logo",
-    src: GriffinMediaLogo,
-    alt: "Griffin Media Logo",
-  },
-  {
     id: 25,
     name: "LA Rams Logo",
     src: LARamsLogo,
@@ -57,18 +48,6 @@ export const ClientLogos: Image[] = [
     name: "NBC Universal Logo",
     src: NBCUniversalLogo,
     alt: "NBC Universal Logo",
-  },
-  {
-    id: 28,
-    name: "Pimco Logo",
-    src: PimcoLogo,
-    alt: "Pimco Logo",
-  },
-  {
-    id: 29,
-    name: "Tegna Logo",
-    src: TegnaLogo,
-    alt: "Tegna Logo",
   },
   {
     id: 30,
