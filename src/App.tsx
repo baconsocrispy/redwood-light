56ec97ea7f893e14a8229acf2a0af1f87f00303f
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-02-24 13:26:16 -0800

    created contact route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Home from "./routes/home/home.component";
 import Navigation from "./components/navigation/navigation.component";
 import Awards from "./routes/awards/awards.component";
 import Team from "./routes/team/team.component";
+import Contact from "./routes/contact/contact.component";
 import NotFound from "./routes/not-found/not-found.component";
 import Footer from "./components/footer/footer.component";
 
@@ -23,6 +24,7 @@ const App = () => {
         <Route index element={ <Home /> } />
         <Route path='/awards' element={ <Awards /> } />
         <Route path='/team' element={ <Team /> } />
+        <Route path='/contact' element={ <Contact /> } />
         <Route path='*' element={ <NotFound />} />
       </Routes>
       <Footer />
