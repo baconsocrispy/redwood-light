f2dd54e25928440f6dd5df9b3e39e3214bb0a145
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-03-01 15:29:29 -0800

    refactored urlPrefix in assets.ts

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,38 +1,40 @@
-// hero image assets
-export const LARams = process.env.PUBLIC_URL +  "images/hero-images/la-rams-digital-studio.jpeg";
-export const KHOUHouston = process.env.PUBLIC_URL + "images/hero-images/khou-houston-studio.jpeg";
-export const NBCMiami = process.env.PUBLIC_URL + "images/hero-images/nbc-miami-studio.jpeg";
-export const SportsNetLA = process.env.PUBLIC_URL + "images/hero-images/sportsnet-la-studio.jpeg";
-export const UFCApex = process.env.PUBLIC_URL + "images/hero-images/ufc-apex-studio.jpeg";
+const urlPrefix = process.env.PUBLIC_URL + 'images/'
 
-// logo image assets
-export const AIGLogo = process.env.PUBLIC_URL +  'images/partner-logos/aig-logo.png';
-export const AmazonLogo = process.env.PUBLIC_URL +  'images/partner-logos/amazon-logo.png';
-export const FoxLogo = process.env.PUBLIC_URL +  'images/partner-logos/fox-logo.png';
-export const LARamsLogo = process.env.PUBLIC_URL +  'images/partner-logos/la-rams-logo.png';
-export const LATimesLogo = process.env.PUBLIC_URL +  'images/partner-logos/la-times-logo.png';
-export const NBCUniversalLogo = process.env.PUBLIC_URL +  'images/partner-logos/nbc-universal-logo.png';
-export const TelemundoLogo = process.env.PUBLIC_URL +  'images/partner-logos/telemundo-logo.png';
-export const TitansLogo = process.env.PUBLIC_URL +  'images/partner-logos/tennessee-titans-logo.png';
-export const UFCLogo = process.env.PUBLIC_URL +  'images/partner-logos/ufc-logo.png';
+// hero image assets
+export const LARams = urlPrefix + "hero-images/la-rams-digital-studio.jpeg";
+export const KHOUHouston = urlPrefix + "hero-images/khou-houston-studio.jpeg";
+export const NBCMiami = urlPrefix + "hero-images/nbc-miami-studio.jpeg";
+export const SportsNetLA = urlPrefix + "hero-images/sportsnet-la-studio.jpeg";
+export const UFCApex = urlPrefix + "hero-images/ufc-apex-studio.jpeg";
+
+// partner logo image assets
+export const AIGLogo = urlPrefix + "partner-logos/aig-logo.png";
+export const AmazonLogo = urlPrefix + "partner-logos/amazon-logo.png";
+export const FoxLogo = urlPrefix + "partner-logos/fox-logo.png";
+export const LARamsLogo = urlPrefix + "partner-logos/la-rams-logo.png";
+export const LATimesLogo = urlPrefix + "partner-logos/la-times-logo.png";
+export const NBCUniversalLogo = urlPrefix + "partner-logos/nbc-universal-logo.png";
+export const TelemundoLogo = urlPrefix + "partner-logos/telemundo-logo.png";
+export const TitansLogo = urlPrefix + "partner-logos/tennessee-titans-logo.png";
+export const UFCLogo = urlPrefix + "partner-logos/ufc-logo.png";
 
 // project image assets
-export const UFC = process.env.PUBLIC_URL + "images/projects/UFC.jpeg";
-export const Rams = process.env.PUBLIC_URL + "images/projects/Rams.jpeg";
-export const Miami = process.env.PUBLIC_URL + "images/projects/Miami.jpeg";
-export const Sportsnet = process.env.PUBLIC_URL + "images/projects/Sportsnet.jpeg";
-export const Dodgers = process.env.PUBLIC_URL + 'images/awards/dodgers.jpeg';
-export const Lakers = process.env.PUBLIC_URL + 'images/awards/lakers.png';
+export const UFC = urlPrefix + "projects/UFC.jpeg";
+export const Rams = urlPrefix + "projects/Rams.jpeg";
+export const Miami = urlPrefix + "projects/Miami.jpeg";
+export const Sportsnet = urlPrefix + "projects/Sportsnet.jpeg";
+export const Dodgers = urlPrefix + "awards/dodgers.jpeg";
+export const Lakers = urlPrefix + "awards/lakers.png";
 
 // service image assets
-export const Broadcast = process.env.PUBLIC_URL + "images/services/broadcast-sportsnet.jpeg";
-export const Corporate = process.env.PUBLIC_URL + "images/services/corporate-amazon.png";
-export const LiveEvents = process.env.PUBLIC_URL + "images/services/esports-live-events-redbull-studio.jpeg";
-export const DesignIntegration1 = process.env.PUBLIC_URL + "images/services/design-integration.png";
-export const DesignIntegration2 = process.env.PUBLIC_URL + "images/services/schematic.png";
+export const Broadcast = urlPrefix + "services/broadcast-sportsnet.jpeg";
+export const Corporate = urlPrefix + "services/corporate-amazon.png";
+export const LiveEvents = urlPrefix + "services/esports-live-events-redbull-studio.jpeg";
+export const DesignIntegration1 = urlPrefix + "services/design-integration.png";
+export const DesignIntegration2 = urlPrefix + "services/schematic.png";
 
 // team image imports
-export const Steve = process.env.PUBLIC_URL +  "images/team/steve.png";
-export const Erik = process.env.PUBLIC_URL +  "images/team/erik.png";
-export const Danny = process.env.PUBLIC_URL +  "images/team/danny.png";
-export const David = process.env.PUBLIC_URL +  "images/team/david.png";
+export const Steve = urlPrefix + "team/steve.png";
+export const Erik = urlPrefix + "team/erik.png";
+export const Danny = urlPrefix + "team/danny.png";
+export const David = urlPrefix + "team/david.png";
