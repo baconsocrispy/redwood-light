5739ae58bb3f2a580efb2dc1a8a87a877ba8922b
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-03-01 10:55:42 -0800

    added images for awards page

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-// internal importss
+// internal imports
 import App from "./App";
 
 // create root DOM element
