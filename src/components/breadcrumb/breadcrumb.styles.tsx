da76d426f53d0cf2b6d9216e032220d20a186a3f
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-03-01 17:00:22 -0800

    fixing breadcrumb issue

--- src/components/breadcrumb/breadcrumb.styles.tsx
+++ src/components/breadcrumb/breadcrumb.styles.tsx
@@ -7,8 +7,8 @@ type BreadcrumbCircleProps = {
 
 export const BreadcrumbCircle = styled.div<BreadcrumbCircleProps>`
   display: inline-block;
-  width: 5px;
-  height: 5px;
+  width: .75rem;
+  height: .75rem;
   background-color: ${ (props) => props.active ? Colors.white : 'none' };
   border: 1px solid ${ Colors.white };
   border-radius: 50%;
